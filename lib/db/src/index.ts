db85f4e487a2791e00ca0beab44f192e2b666e8c
rahma allam <rahmaabdelraoufallam@gmail.com>
2026-05-16 09:12:19 +0300

    first commit

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema";
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
